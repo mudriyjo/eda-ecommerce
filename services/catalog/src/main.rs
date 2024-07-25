4cf2cce21d8f461deae1344a2a74dc4aca1d9042
mudriyjo <mudriyjo@gmail.com>
2024-07-26 00:07:03 +0400

    init first order service

---
+++ services/catalog/src/main.rs
@@ -0,0 +1,52 @@
+use axum::{response::Html, routing::get, Extension, Json};
+use sqlx::PgPool;
+use utoipa::OpenApi;
+use utoipa_swagger_ui::SwaggerUi;
+
+async fn index() -> Html<String> {
+    Html("<h1>Hello world!</h1>".to_string())
+}
+
+#[derive(OpenApi)]
+#[openapi(paths(openapi))]
+struct ApiDoc;
+
+#[utoipa::path(
+    get,
+    path = "/api-docs/openapi.json",
+    responses(
+        (status = 200, description = "JSON file", body = ())
+    )
+)]
+async fn openapi() -> Json<utoipa::openapi::OpenApi> {
+    Json(ApiDoc::openapi())
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    dotenv::dotenv().expect("Can't find .env file or variables");
+
+    let server_port_address = std::env::var("SERVER")?;
+    let db_url = std::env::var("DATABASE_URL")?;
+    let pool = PgPool::connect(&db_url).await?;
+
+    sqlx::migrate!("./migrations").run(&pool).await?;
+
+    color_eyre::install().expect("Error with starting color eyre hook...");
+
+    tracing_subscriber::fmt::init();
+
+    let extension_pool = pool.clone();
+    let connection = tokio::net::TcpListener::bind(server_port_address).await?;
+
+    let router = axum::Router::new()
+        .route("/", get(index))
+        .route("/swagger/openapi.json", get(openapi))
+        .merge(SwaggerUi::new("/swagger-ui"))
+        .layer(Extension(extension_pool));
+
+    tracing::info!("Start server...");
+    axum::serve(connection, router).await?;
+
+    Ok(())
+}
