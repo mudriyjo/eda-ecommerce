5f0abaeb2fe48fed89f148678a0eb4e3b6c5d368
mudriyjo <mudriyjo@gmail.com>
2024-07-26 17:52:08 +0400

    set up kafka consumer and info debug

--- services/catalog/src/main.rs
+++ services/catalog/src/main.rs
@@ -1,5 +1,7 @@
 use axum::{response::Html, routing::get, Extension, Json};
 use env::Env;
+use futures::StreamExt;
+use rdkafka::{consumer::{Consumer, StreamConsumer}, ClientConfig};
 use sqlx::PgPool;
 use utoipa::OpenApi;
 use utoipa_swagger_ui::SwaggerUi;
@@ -25,17 +27,38 @@ async fn openapi() -> Json<utoipa::openapi::OpenApi> {
     Json(ApiDoc::openapi())
 }
 
+async fn listening_kafka_topic(kafka_consumer: &StreamConsumer) {
+    kafka_consumer.stream().for_each(|msg| {
+        async move {
+            println!("{:?}", msg);
+        }
+    }).await;
+}
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    tracing_subscriber::fmt::init();
+
+    tracing::info!("Read env variables.");
     let env = Env::new()?;
 
+    tracing::info!("Connection to PostgreSQL.");
     let pool = PgPool::connect(&env.db_connection_string).await?;
+    
+    tracing::info!("Connection to Kafka.");
+    let kafka_consumer: StreamConsumer = ClientConfig::new()
+        .set("group.id", env.kafka_group_id)
+        .set("bootstrap.servers", env.kafka_broker)
+        .set("session.timeout.ms", "6000")
+        .create()?;
 
+    kafka_consumer.subscribe(&["example"])?;
+    // listening_kafka_topic(&kafka_consumer).await;
+
+    tracing::info!("Execute migrations.");
     sqlx::migrate!("./migrations").run(&pool).await?;
 
+    tracing::info!("Set uping hook for panic.");
     color_eyre::install().expect("Error with starting color eyre hook...");
-
-    tracing_subscriber::fmt::init();
 
     let extension_pool = pool.clone();
     let connection = tokio::net::TcpListener::bind(env.server_address).await?;
